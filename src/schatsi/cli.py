671f0aba602c40c16d7873fa0e1efe993731c8c9
Felix Schramm <felixschramm@live.de>
2023-08-17 13:16:54 +0200

    added a message for not prociding input path

--- src/schatsi/cli.py
+++ src/schatsi/cli.py
@@ -4,22 +4,27 @@ import nltk
 
 from schatsi.jobs import *
 
-nltk.download("punkt")
-
-
 @click.command()
-@click.option('--input_path', help='The path where all files to process can be found.')
-@click.option('--output_path', help='The path where results are put')
-@click.option('--functional_terms', help='Path to a CSV with all funtional termns')
-@click.option('--negative_terms', help='Path to a CSV with all negative termns')
-@click.option('--parallel', default=True, help='Prallel processing or not')
-def cli(input_path, output_path, functional_terms, negative_terms, parallel):
-    if parallel:
-        job = ParallelJob(input_path, output_path, functional_terms, negative_terms)
+@click.option('-i', '--input_path', help='The path where all files to process can be found.')
+@click.option('-o', '--output_path', help='The path where results are put')
+@click.option('-f', '--functional_terms', help='Path to a CSV with all funtional termns')
+@click.option('-n', '--negative_terms', help='Path to a CSV with all negative termns')
+@click.option('-p', '--parallel', default=True, help='Prallel processing or not')
+@click.option('-m', '--menu', 'menu', help="Show a guided menu to select the options", flag_value=True)
+def cli(input_path, output_path, functional_terms, negative_terms, parallel, menu):
+    if menu:
+        print("This is where the menu will be")
     else:
-        job = SingleJob(input_path, output_path, functional_terms, negative_terms)
-    
-    job.process()
+        pass
+    if input_path:
+        nltk.download("punkt")
+        if parallel:
+            job = ParallelJob(input_path, output_path, functional_terms, negative_terms)
+        else:
+            job = SingleJob(input_path, output_path, functional_terms, negative_terms)
+        job.process()
+    else:
+        print("You have not provided an input path. Please use the -i option to provide one.")
 
 if __name__ == "__main__":
     job = ParallelJob("data/input", "data/output", "data/metadata/functional_terms.csv", "data/metadta/negative_terms.csv")
